832e3d49a6a5f98b63ae2ac0b5efd8f396922efe
Min Chen <dontminchenit@gmail.com>
2020-06-26 15:24:43 -0400

    Updated with main function and comments

--- AOQualitySorter.m
+++ AOQualitySorter.m
@@ -1,13 +1,21 @@
-%inDir = 'D:\Research\ResearchResults\AO\QualitySorter\Data\13131_20150630_OD\13131_20150630_OD';
-%outDir = 'D:\Research\ResearchResults\AO\QualitySorter\EdgeTest_6_6_2020';
+function AOQualitySorter()
+addpath(genpath('.'));
 
-%inDir = 'D:\Research\ResearchResults\AO\synthesis\AOdata\RP_13153_20150326_OD_Images-DONE\Montaged_Individual';
-%inDir = 'D:\Research\ResearchResults\AO\QualitySorter\Data\13131_20150630_OD\13131_20150630_OD';
-%outDir = 'D:\Research\ResearchResults\AO\QualitySorter\Results\FullTests_6_18_2020\13131_20150630_OD';
+inDir=uigetdir('.','Select input image directory:');
+if (inDir == 0)
+    disp('Error: No input image path selected')   
+    return
+else
+    disp(['Input image path selected: ' inDir])
+end
 
-inDir = 'D:\Research\ResearchResults\AO\QualitySorter\Data\13125_OD_20150312_clean';
-outDir = 'D:\Research\ResearchResults\AO\QualitySorter\Results\FullTests_6_18_2020\13125_OD_20150312';
-mkdir(outDir);
+outDir=uigetdir(inDir,'Select where to save sorted images:');
+if (outDir == 0)
+    disp('Error:No output save path selected')   
+    return
+else
+    disp(['Output save path selected: ' outDir])
+end
 
 %Load Data and calculate edge score
 AllSplit = dir(fullfile(inDir,'*split*'));
@@ -17,6 +25,7 @@ score = zeros(1,N);
 imMasksOverlap = cell(1,N);%This stores the mask of each image as a vector 
                             %subscript and their pairwise overlap
 for i = 1:N
+    disp(['Calculating quality measures: ' num2str(i) ' of ' num2str(N)])
     im = imread(fullfile(inDir,AllSplit(i).name));
     mask = im(:,:,1)<255;
     imMasksOverlap{i,i} = find(mask(:));
@@ -26,6 +35,7 @@ end
 
 %calculate pair wise overlap
 for i = 1:N
+    disp(['Calculating pairwise overlaps: ' num2str(i) ' of ' num2str(N)])
     for j = (i+1):N
        imMasksOverlap{i,j} = intersect(imMasksOverlap{i,i},imMasksOverlap{j,j});
        imMasksOverlap{j,i} = imMasksOverlap{i,j};
@@ -38,6 +48,7 @@ I_new = CondenseOrdering(score,imMasksOverlap);
 
 %save out new ordering
 for i = 1:N
+    disp(['Saving new ordering: ' num2str(i) ' of ' num2str(N)])
     fnameIn = AllSplit(I_new(i)).name;
     fnameOut = [num2str(i,'%04.f') '_' AllSplit(I_new(i)).name];
    copyfile(fullfile(inDir,fnameIn),fullfile(outDir,fnameOut));
@@ -53,4 +64,5 @@ for i = 1:N
    fnameOut2 = strrep(fnameOut, '_split_det_', '_confocal_');
    fnameOut2 = strrep(fnameOut2, '_m2.', '_m3.');
    copyfile(fullfile(inDir,fnameIn2),fullfile(outDir,fnameOut2));
+end
 end
